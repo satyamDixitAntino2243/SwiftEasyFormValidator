71483b6433fd1c36af8d3b0777a0387d0c0f9242
Satyam Dixit <satyam.d@antino.io>
2023-03-16 15:47:50 +0530

    up

---
+++ SwiftEasyFormValidator/SwiftEasyFormValidator.h
@@ -0,0 +1,18 @@
+//
+//  SwiftEasyFormValidator.h
+//  SwiftEasyFormValidator
+//
+//  Created by satyam dixit on 16/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftEasyFormValidator.
+FOUNDATION_EXPORT double SwiftEasyFormValidatorVersionNumber;
+
+//! Project version string for SwiftEasyFormValidator.
+FOUNDATION_EXPORT const unsigned char SwiftEasyFormValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEasyFormValidator/PublicHeader.h>
+
+
---
+++ SwiftEasyFormValidator/Validator.swift
@@ -0,0 +1,14 @@
+//
+//  Validator.swift
+//  SwiftEasyFormValidator
+//
+//  Created by satyam dixit on 16/03/23.
+//
+
+import Foundation
+
+public struct Validator {
+    public static func sayHi(){
+        print("Hi Droomersss, How rr youuuuuuu!!!!!")
+    }
+}
